69677236c7e1448d924562a94583bbc3dfff82d0
lvtongda <lvtongda@gmail.com>
2014-05-25 23:42:06 +0800

    Encapsulation to SDK

---
+++ src/com/UpYun.java
@@ -0,0 +1,139 @@
+/*
+ * Filename: UpYun.java
+ * Author: lvtongda 
+ * Date: 2014-05-24
+ * About: JAVA 版本缓存刷新 SDK
+ */
+
+package com;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.Consts;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+
+public class UpYun {
+	
+	/** 默认的编码格式 */
+	private static final String UTF8 = "UTF-8";
+
+	/** SDK 版本号*/
+	private static String VERSION = "2.0";
+	
+	private static String EXPECT = "Expect";
+	private static String AUTHORIZATION = "Authorization";
+	private static String DATE = "Date";
+	
+	public static String PURGE_API = "http://purge.upyun.com/purge/";
+	
+	// 空间名
+	protected String bucketName = null; 
+	// 操作员名
+	protected String userName = null; 
+	// 操作员密码
+	protected String password = null; 
+	
+	/**
+	 * 初始化 UpYun 接口
+	 * @param bucketName
+	 * @param userName
+	 * @param password
+	 */
+	public UpYun(final String bucketName, final String userName, final String password) {
+		this.bucketName = bucketName;
+		this.userName = userName;
+		this.password = md5(password);	
+	}
+	
+	/**
+	 * 获取当前 SDK 的版本号
+	 * @return
+	 */
+	public String version() {
+		return VERSION;
+	}
+	
+	/**
+	 * 刷新缓存
+	 * @param url
+	 * @return
+	 */
+	public String purgeUrl(String url) {
+		String result = null;
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPost post = new HttpPost(PURGE_API);
+		
+		String sign = md5(url + "&" + bucketName + "&" + getGMTDate() + "&" + password);
+		post.setHeader(EXPECT, "");
+		post.setHeader(AUTHORIZATION, "UpYun " + bucketName + ":" + userName + ":" + sign);
+		post.setHeader(DATE, getGMTDate());
+		
+		
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("purge", url));
+		post.setEntity(new UrlEncodedFormEntity(params, Consts.UTF_8));
+		HttpResponse response;
+		try {
+			response = client.execute(post);
+			result = EntityUtils.toString(response.getEntity());	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 对字符串进行 MD5 加密
+	 * @param strSrc 
+	 * @return
+	 */
+	public static String md5(String strSrc) {
+			String result = "";
+			byte[] temp = null;
+			MessageDigest md5 = null;
+			try {
+				md5 = MessageDigest.getInstance("MD5");
+			} catch (NoSuchAlgorithmException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				temp = md5.digest(strSrc.getBytes(UTF8));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+			
+			for (int i = 0; i < temp.length; i++) {
+				result += Integer.toHexString((0x000000ff & temp[i]) | 0xffffff00).substring(6);
+			}
+			
+			return result;
+	}
+	
+	/**
+	 * 获取 GMT 格式时间戳 
+	 * @return
+	 */
+	private String getGMTDate() {
+		SimpleDateFormat df = new SimpleDateFormat(
+				"E, dd MMM yyyy HH:mm:ss 'GMT'", Locale.US);
+		df.setTimeZone(TimeZone.getTimeZone("GMT"));
+		return df.format(new Date());
+	}
+}
---
+++ src/demo/PurgeDemo.java
@@ -0,0 +1,30 @@
+package demo;
+
+import com.UpYun;
+
+public class PurgeDemo {
+	// 运行前先设置以下参数
+	private static final String BUCKET_NAME = "空间名";
+	private static final String USER_NAME = "操作员名";
+	private static final String USER_PWD = "密码";
+	
+	// 要刷新的 URL
+	// URL 样式：http://ceshi-images.b0.upaiyun.com/100.jpg\nhttp://ceshi-images.b0.upaiyun.com/100.jpg\n
+	private static String URL = "";
+	
+	private static UpYun upyun = null;
+	
+	public static void main(String[] args) {
+		
+		// 初始化空间
+		upyun = new UpYun(BUCKET_NAME, USER_NAME, USER_PWD);
+	
+		// 刷新缓存
+		testPurgeUrl();
+	}
+	
+	public static void testPurgeUrl() {
+		String result = upyun.purgeUrl(URL);
+		System.out.println(result);
+	}
+}
